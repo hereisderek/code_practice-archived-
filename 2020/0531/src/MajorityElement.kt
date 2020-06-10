6e600bb44f8da4fd9cf27b6e57714ae322e2aeb6
Derek Zhu <derek.zhu@genesisenergy.co.nz>
2020-06-11 00:04:35 +1200

    MajorityElement https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3321/

--- 2020/0531/src/MajorityElement.kt
+++ 2020/0531/src/MajorityElement.kt
@@ -1,3 +1,5 @@
+import kotlin.test.assertEquals
+
 // Majority Element
 // https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3321/
 
@@ -9,6 +11,67 @@
  *
  * */
 
+fun main() {
+    assertEquals(3, MajorityElement.majorityElement(intArrayOf(3, 2, 3)))
+    assertEquals(2, MajorityElement.majorityElement(intArrayOf(2, 2, 1, 1, 1, 2, 2)))
+}
+
 object MajorityElement {
-    
+
+    /// sample
+    fun majorityElement(nums: IntArray): Int {
+        var count = 0
+        var candidate: Int? = null
+        for (num in nums) {
+            if (count == 0) {
+                candidate = num
+            }
+            count += if (num == candidate) 1 else -1
+        }
+        return candidate!!
+    }
+
+
+    /// my solutions:
+    fun majorityElement4(nums: IntArray): Int {
+        val counters = IntArray(nums.size)
+        nums.forEachIndexed { _, num ->
+            val index = nums.indexOfFirst { it == num }
+            counters[index] += 1
+        }
+        var maxIndex = 0
+        var max = -1
+        counters.forEachIndexed { index, i ->
+            if (i == 0) {
+                return@forEachIndexed
+            }
+            if (i > max) {
+                max = i
+                maxIndex = index
+            }
+        }
+        return nums[maxIndex]
+    }
+
+
+    fun majorityElement3(nums: IntArray): Int {
+        val intMap = HashMap<Int, Int>(nums.size)
+        nums.forEach {
+            if (intMap.containsKey(it)) {
+                intMap[it] = intMap[it]!! + 1
+            } else {
+                intMap[it] = 1
+            }
+        }
+        val halfPoint = nums.size / 2f
+        intMap.forEach {
+            if (it.value > halfPoint) return it.key
+        }
+        return -1
+    }
+
+    fun majorityElement2(nums: IntArray): Int = nums.run {
+        sort()
+        nums[nums.size / 2]
+    }
 }
